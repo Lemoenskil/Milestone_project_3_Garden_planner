6d4e0c73926b8de75f7b514bd582e00a79ac3e50
Yvette Smith <yvette@boesman.nl>
2020-01-12 18:02:14 +0000

    add procfile

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+
 
 app = Flask ("----name----")
 @app.route('/')
